00c1f5ecee07c8d8d61e29ec57f427315c13515e
naufalhisyam <naufalhisyam46@gmail.com>
2024-11-16 11:12:49 +0700

    add OutputUtils.java

---
+++ src/main/java/utils/OutputUtils.java
@@ -0,0 +1,45 @@
+package utils;
+
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+public class OutputUtils {
+    private static PrintWriter pw;
+
+    private OutputUtils() {}
+
+    public static void initPrintWriter() {
+        if (pw == null) {
+            pw = new PrintWriter(System.out);
+        }
+    }
+
+    public static void printArray(byte[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void printArray(int[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void printArray(double[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void printArray(long[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void printArray(boolean[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void printArray(char[] array) {
+        pw.println(Arrays.toString(array));
+    }
+
+    public static void closePrintWriter() {
+        pw.flush();
+        pw.close();
+    }
+}
