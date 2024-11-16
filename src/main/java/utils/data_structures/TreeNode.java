0f215707b6a58ac18ec19e02b97de9816dd3cb19
naufalhisyam <naufalhisyam46@gmail.com>
2024-11-16 23:18:44 +0700

    add toString() method to TreeNode class

--- src/main/java/utils/data_structures/TreeNode.java
+++ src/main/java/utils/data_structures/TreeNode.java
@@ -1,5 +1,8 @@
 package utils.data_structures;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class TreeNode {
     public int val;
     public TreeNode left;
@@ -13,5 +16,32 @@ public class TreeNode {
         this.val = val;
         this.left = left;
         this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        queue.addLast(this);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.removeFirst();
+            sb.append(node.val);
+
+            if (node.left != null) {
+                queue.addLast(node.left);
+            }
+            if (node.right != null) {
+                queue.addLast(node.right);
+            }
+
+            if (!queue.isEmpty()) {
+                sb.append(",");
+            }
+        }
+
+        sb.append("]");
+        return sb.toString();
     }
 }
